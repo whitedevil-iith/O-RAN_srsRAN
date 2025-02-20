875f0d67f671d4ddfc8bcd931095fe575dc262dc
L S Yaswanth Kumar <cs24resch11007@iith.ac.in>
2025-02-21 01:47:00 +0530

    Updated containerStateChecker.py

--- containerStateChecker/containerStateChecker.py
+++ containerStateChecker/containerStateChecker.py
@@ -7,18 +7,18 @@ import subprocess
 
 # Define the screen command to reattach to the 'RAN' session and send commands
 commands = [
-    "screen -r OTHERS -X 'ps aux | grep 'stresser.py' | awk '{print $2}' | xargs kill\n'",
-    "screen -r OTHERS -X 'ps aux | grep 'trafficGenerator.py' | awk '{print $2}' | xargs kill\n'",
-    "screen -r OTHERS -X 'ps aux | grep 'dataScrapper.py' | awk '{print $2}' | xargs kill\n'",
-    "screen -r RAN -X stuff 'docker compose down\n'",  # Run docker compose down RAN
-    "screen -r RAN -X stuff 'docker compose up\n'",     # Run docker compose up RAN
-    "screen -r RIC -X stuff 'docker compose down\n'",  # Run docker compose down RIC
-    "screen -r RIC -X stuff 'docker compose up\n'",     # Run docker compose up RIC
-    "screen -r MON -X stuff 'docker rm -f prometheus cadvisor node-exporter\n'",
-    "screen -r MON -X stuff 'docker pull prom/prometheus:latest && docker run -d --name=prometheus --network=oran-intel -p 9090:9090 -v=$PWD/setup/prometheus:/prometheus-data prom/prometheus:latest --config.file=/prometheus-data/prometheus.yml && docker pull gcr.io/cadvisor/cadvisor:latest && docker run --name=cadvisor --network=oran-intel --volume=/:/rootfs:ro --volume=/var/run:/var/run:rw --volume=/sys:/sys:ro --volume=/var/lib/docker/:/var/lib/docker:ro --publish=8080:8080 --detach=true gcr.io/cadvisor/cadvisor:latest && docker run -d --name=node-exporter --network=oran-intel -p 9100:9100 prom/node-exporter:latest\n'",
-    "screen -r OTHERS -X 'python3 stresser/stresser.py &\n'",
-    "screen -r OTHERS -X 'python3 trafficGenerator/trafficGenerator.py &\n'",
-    "screen -r OTHERS -X 'python3 dataScrapper/dataScrapper.py &\n'",
+    "screen -r OTHERS -X 'cd /home/newsl/workspace/O-RAN_srsRAN && ps aux | grep 'stresser.py' | awk '{print $2}' | xargs kill -9\n'",
+    "screen -r OTHERS -X 'cd /home/newsl/workspace/O-RAN_srsRAN &&  ps aux | grep 'trafficGenerator.py' | awk '{print $2}' | xargs kill -9\n'",
+    "screen -r OTHERS -X 'cd /home/newsl/workspace/O-RAN_srsRAN && ps aux | grep 'dataScrapper.py' | awk '{print $2}' | xargs kill -9\n'",
+    "screen -r RAN -X stuff 'cd /home/newsl/workspace/O-RAN_srsRAN && docker compose down\n'",  # Run docker compose down RAN
+    "screen -r RAN -X stuff 'cd /home/newsl/workspace/O-RAN_srsRAN && docker compose up\n'",     # Run docker compose up RAN
+    "screen -r RIC -X stuff 'cd /home/newsl/workspace/O-RAN_srsRAN/RIC/oran-sc-ric && docker compose down\n'",  # Run docker compose down RIC
+    "screen -r RIC -X stuff 'cd /home/newsl/workspace/O-RAN_srsRAN/RIC/oran-sc-ric && docker compose up\n'",     # Run docker compose up RIC
+    "screen -r MON -X stuff 'cd /home/newsl/workspace/O-RAN_srsRAN && docker rm -f prometheus cadvisor node-exporter\n'",
+    "screen -r MON -X stuff 'cd /home/newsl/workspace/O-RAN_srsRAN && docker pull prom/prometheus:latest && docker run -d --name=prometheus --network=oran-intel -p 9090:9090 -v=$PWD/setup/prometheus:/prometheus-data prom/prometheus:latest --config.file=/prometheus-data/prometheus.yml && docker pull gcr.io/cadvisor/cadvisor:latest && docker run --name=cadvisor --network=oran-intel --volume=/:/rootfs:ro --volume=/var/run:/var/run:rw --volume=/sys:/sys:ro --volume=/var/lib/docker/:/var/lib/docker:ro --publish=8080:8080 --detach=true gcr.io/cadvisor/cadvisor:latest && docker run -d --name=node-exporter --network=oran-intel -p 9100:9100 prom/node-exporter:latest\n'",
+    "screen -r OTHERS -X 'cd /home/newsl/workspace/O-RAN_srsRAN && python3 stresser/stresser.py &\n'",
+    "screen -r OTHERS -X 'cd /home/newsl/workspace/O-RAN_srsRAN && python3 trafficGenerator/trafficGenerator.py &\n'",
+    "screen -r OTHERS -X 'cd /home/newsl/workspace/O-RAN_srsRAN && python3 dataScrapper/dataScrapper.py &\n'",
 ]
     
 def monitor_containers():
@@ -37,6 +37,7 @@ def monitor_containers():
                 if container_name not in running_containers:
                     print(f"{timestamp} WARNING: Container '{container_name}' is not running.")
                     # Execute the commands in sequence
+                    print("Running commands")
                     for command in commands:
                         result = subprocess.run(command, capture_output=True, text=True, shell=True)
                     # return
@@ -47,18 +48,21 @@ def monitor_containers():
                         if log_keyword in logs:
                             print(f"{timestamp} WARNING: Found '{log_keyword}' in logs of container '{container_name}'.")
                             # Execute the commands in sequence
+                            print("Running commands")
                             for command in commands:
                                 result = subprocess.run(command, capture_output=True, text=True, shell=True)
                             # return
                     except subprocess.CalledProcessError:
                         print(f"{timestamp} WARNING: Failed to retrieve logs for container '{container_name}'.")
                         # Execute the commands in sequence
+                        print("Running commands")
                         for command in commands:
                             result = subprocess.run(command, capture_output=True, text=True, shell=True)
                         # return
         except Exception as e:
             print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]} An error occurred: {e}")
             # Execute the commands in sequence
+            print("Running commands")
             for command in commands:
                 result = subprocess.run(command, capture_output=True, text=True, shell=True)
             # return
